26d847f8488f6f535644a8f2288cd428005b8f69
Caspel26 <giuseppework26@gmail.com>
2025-10-04 12:54:53 +0200

    feat: add buttons for keys management, passwords management, and about section in HomeView

--- electron-app/src/views/HomeView.vue
+++ electron-app/src/views/HomeView.vue
@@ -1,6 +1,21 @@
 <template>
   <main>
-    <Header/>
+      <Header />
+      <v-container class="mt-12">
+        <v-row align="center" justify="center" class="text-center">
+          <v-col cols="12" class="mt-8">
+        <v-btn block rounded="lg" color="primary" prepend-icon="mdi-key">
+          Keys management
+        </v-btn>
+          </v-col>
+          <v-col cols="12" class="mt-8">
+        <v-btn block rounded="lg" color="primary" prepend-icon="mdi-lock">Passwords management</v-btn>
+          </v-col>
+          <v-col cols="12" class="mt-8">
+        <v-btn block rounded="lg" color="primary" prepend-icon="mdi-information">About</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
   </main>
 </template>
 
