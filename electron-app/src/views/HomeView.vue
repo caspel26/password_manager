2227bfcb060ae12d2c62e83753a5df9e384d2dd7
Caspel26 <giuseppework26@gmail.com>
2025-10-04 13:05:10 +0200

    style: enhance button elevation for keys, passwords, and about sections in HomeView

--- electron-app/src/views/HomeView.vue
+++ electron-app/src/views/HomeView.vue
@@ -4,15 +4,15 @@
       <v-container class="mt-12">
         <v-row align="center" justify="center" class="text-center">
           <v-col cols="12" class="mt-8">
-        <v-btn block rounded="lg" color="primary" prepend-icon="mdi-key">
+        <v-btn block rounded="lg" color="primary" class="elevation-7" prepend-icon="mdi-key">
           Keys management
         </v-btn>
           </v-col>
           <v-col cols="12" class="mt-8">
-        <v-btn block rounded="lg" color="primary" prepend-icon="mdi-lock">Passwords management</v-btn>
+        <v-btn block rounded="lg" color="primary" class="elevation-7" prepend-icon="mdi-lock">Passwords management</v-btn>
           </v-col>
           <v-col cols="12" class="mt-8">
-        <v-btn block rounded="lg" color="primary" prepend-icon="mdi-information">About</v-btn>
+        <v-btn block rounded="lg" color="primary" class="elevation-7" prepend-icon="mdi-information">About</v-btn>
           </v-col>
         </v-row>
       </v-container>
